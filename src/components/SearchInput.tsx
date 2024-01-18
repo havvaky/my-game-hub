0dc980a56b11be98dacca887dd6ef1b95a0026c4
havvaky <havvak@yahoo.com>
2024-01-18 14:44:57 -0500

    implementing search funtionality

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,14 +1,34 @@
 import { SearchIcon } from "@chakra-ui/icons";
 import { InputGroup, InputLeftElement, Input } from "@chakra-ui/react";
+import { useRef } from "react";
 
-const SearchInput = () => {
+interface Props {
+  onSearch: (searchText: string) => void;
+}
+
+const SearchInput = ({ onSearch }: Props) => {
+  const ref = useRef<HTMLInputElement>(null);
   return (
-    <InputGroup>
-      <InputLeftElement pointerEvents="none">
-        <SearchIcon color="gray.300" />
-      </InputLeftElement>
-      <Input placeholder="Search games" borderRadius={20} variant="filled" />
-    </InputGroup>
+    <form
+      onSubmit={(event) => {
+        event?.preventDefault();
+        if (ref.current) {
+          onSearch(ref.current.value);
+        }
+      }}
+    >
+      <InputGroup>
+        <InputLeftElement pointerEvents="none">
+          <SearchIcon color="gray.300" />
+        </InputLeftElement>
+        <Input
+          ref={ref}
+          placeholder="Search games"
+          borderRadius={20}
+          variant="filled"
+        />
+      </InputGroup>
+    </form>
   );
 };
 
