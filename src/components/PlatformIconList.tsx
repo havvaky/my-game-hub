2319221e263f4103671e29fa2b4520bbc50dfa05
havvaky <havvak@yahoo.com>
2024-01-18 11:44:18 -0500

    fixing console errors

--- src/components/PlatformIconList.tsx
+++ src/components/PlatformIconList.tsx
@@ -32,7 +32,7 @@ const PlatformIconList = ({ platforms }: Props) => {
   return (
     <HStack marginY={1}>
       {platforms.map((platform) => (
-        <Icon as={iconList[platform.slug]} color="gray.500" />
+        <Icon key={platform.id} as={iconList[platform.slug]} color="gray.500" />
       ))}
     </HStack>
   );
