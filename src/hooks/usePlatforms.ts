6607872667064736830a1762593afad7a5bfc27b
havvaky <havvak@yahoo.com>
2024-01-18 20:20:51 -0500

    ship static data for genres with the app

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,4 +1,4 @@
-import useData from "./useData";
+import genres from "../data/genres"
 
 export interface Platform {
     id: number;
@@ -6,6 +6,6 @@ export interface Platform {
     slug: string;
 }
 
-const usePlatforms = () => useData<Platform>('/platforms/lists/parents')
+const usePlatforms = () => ({ data: genres})
 
 export default usePlatforms;
